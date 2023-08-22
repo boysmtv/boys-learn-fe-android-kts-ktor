92ceea8e99666f85d7fb37e1bf50663469d58705
boysmtv <boys.mtv@gmail.com>
2023-08-22 15:36:23 +0700

    clear project

--- app/src/main/java/com/kotlin/learn/MainActivity.kt
+++ app/src/main/java/com/kotlin/learn/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import com.kotlin.learn.core.common.util.isMyServiceRunning
 import com.kotlin.learn.core.common.util.security.DataStorePreferences
 import com.kotlin.learn.feature.services.ProfileService
@@ -27,8 +28,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
-        navHostFragment
-
         setupProfileService()
     }
 
